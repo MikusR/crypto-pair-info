91f8cd4f9320570f42db41a48ac9119e340b31fe
MikusR <mikus.rakis@gmail.com>
2023-10-27 16:30:53 +0300

    init

--- app/CoinPair.php
+++ app/CoinPair.php
@@ -21,32 +21,23 @@ class CoinPair
         $this->lowPrice = $lowPrice;
     }
 
-    /**
-     * @return string
-     */
+
     public function getSymbol(): string
     {
         return $this->symbol;
     }
 
-    /**
-     * @return float
-     */
+
     public function getPriceChange(): float
     {
         return $this->priceChange;
     }
-
-
-rn float
 
     public function getHighPrice(): float
     {
         return $this->highPrice;
     }
 
-
-turn float
 
     public function getLowPrice(): float
     {
